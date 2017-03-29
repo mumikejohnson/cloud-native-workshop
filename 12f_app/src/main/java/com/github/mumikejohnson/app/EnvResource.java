3e5c76aa8c2c1039f6ae46297ca39e605a95ec15
Mike T Johnson <MikeT.Johnson@sas.com>
2017-03-29 16:21:15 -0400

    sometihng

---
+++ 12f_app/src/main/java/com/github/mumikejohnson/app/EnvResource.java
@@ -0,0 +1,20 @@
+package com.github.mumikejohnson.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvResource {
+	@Autowired
+	private Environment environment;
+	
+	@RequestMapping(value = "/get-env", method = RequestMethod.GET)
+	public String getEnv(@RequestParam String env) {
+		
+	    return "Env" + " " + environment.getProperty(env);
+	}
+}
